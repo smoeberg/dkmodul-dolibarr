a2279e224752bd27175f8c0a2dc866e8a37fe0be
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-18 21:17:51 +0200

    feat: retain local bookkeeping identity for applied SAF-T transactions

--- dkmodul/sql/llx_dk_saft_import_transaction.sql
+++ dkmodul/sql/llx_dk_saft_import_transaction.sql
@@ -8,6 +8,9 @@ CREATE TABLE llx_dk_saft_import_transaction (
     registration_datetime DATETIME NOT NULL,
     actor VARCHAR(255) NOT NULL,
     description VARCHAR(500) NULL,
+    local_piece_num INTEGER NULL,
+    local_ref VARCHAR(30) NULL,
     UNIQUE KEY uk_dk_saft_import_transaction (fk_import, external_transaction_id),
-    KEY idx_dk_saft_import_transaction_date (fk_import, transaction_date)
+    KEY idx_dk_saft_import_transaction_date (fk_import, transaction_date),
+    KEY idx_dk_saft_import_transaction_piece (local_piece_num)
 ) ENGINE=InnoDB;
