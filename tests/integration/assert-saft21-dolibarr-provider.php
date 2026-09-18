d752e0df8c0a4b766718775b427df04016f295fd
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-18 22:26:35 +0200

    fix: assert VAT mapping by local tax code instead of TaxTable order

--- tests/integration/assert-saft21-dolibarr-provider.php
+++ tests/integration/assert-saft21-dolibarr-provider.php
@@ -44,7 +44,7 @@ $checks = array(
     'string(/saf:AuditFile/saf:Header/saf:Company/saf:CVR)' => '12345678',
     'string(/saf:AuditFile/saf:Header/saf:DefaultCurrencyCode)' => 'DKK',
     'string(/saf:AuditFile/saf:MasterFiles/saf:GeneralLedgerAccounts/saf:VersionOfStandardAccount)' => '20260101',
-    'string(/saf:AuditFile/saf:MasterFiles/saf:TaxTable/saf:TaxTableEntry/saf:TaxCodeDetails/saf:StandardTaxCode)' => 'S1',
+    'string(/saf:AuditFile/saf:MasterFiles/saf:TaxTable/saf:TaxTableEntry/saf:TaxCodeDetails[saf:TaxCode="DKTEST25"]/saf:StandardTaxCode)' => 'S1',
     'string(/saf:AuditFile/saf:GeneralLedgerEntries/saf:NumberOfEntries)' => '13',
     'string(/saf:AuditFile/saf:GeneralLedgerEntries/saf:Journal/saf:Transaction[saf:TransactionID="DK-990003"]/saf:Line[saf:AccountID="3000"]/saf:TaxInformation/saf:TaxCode)' => 'DKTEST25',
     'string(/saf:AuditFile/saf:GeneralLedgerEntries/saf:Journal/saf:Transaction[saf:TransactionID="DK-990003"]/saf:Line[saf:AccountID="3000"]/saf:TaxInformation/saf:StandardTaxCode)' => 'S1',
