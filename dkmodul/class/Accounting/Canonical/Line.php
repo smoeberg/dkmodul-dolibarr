106f035eda2e7a9d83a938623216fc86608bd9df
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-18 12:49:35 +0200

    feat: add dkmodul/class/Accounting/Canonical/Line.php

---
+++ dkmodul/class/Accounting/Canonical/Line.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__.'/Decimal.php';
+
+class DkCanonicalLine
+{
+    public $lineId;
+    public $accountCode;
+    public $debit;
+    public $credit;
+    public $partyId;
+    public $taxCode;
+    public $currencyCode;
+    public $currencyAmount;
+    public $description;
+    public $sourceDocumentRef;
+
+    public function __construct(array $data)
+    {
+        $this->lineId = (string) ($data['lineId'] ?? '');
+        $this->accountCode = (string) ($data['accountCode'] ?? '');
+        $this->debit = DkCanonicalDecimal::normalize($data['debit'] ?? '0');
+        $this->credit = DkCanonicalDecimal::normalize($data['credit'] ?? '0');
+        $this->partyId = isset($data['partyId']) ? (string) $data['partyId'] : null;
+        $this->taxCode = isset($data['taxCode']) ? (string) $data['taxCode'] : null;
+        $this->currencyCode = isset($data['currencyCode']) ? (string) $data['currencyCode'] : null;
+        $this->currencyAmount = isset($data['currencyAmount'])
+            ? DkCanonicalDecimal::normalize($data['currencyAmount'])
+            : null;
+        $this->description = isset($data['description']) ? (string) $data['description'] : null;
+        $this->sourceDocumentRef = isset($data['sourceDocumentRef']) ? (string) $data['sourceDocumentRef'] : null;
+
+        if ($this->lineId === '') {
+            throw new InvalidArgumentException('Canonical line id is required');
+        }
+        if ($this->accountCode === '') {
+            throw new InvalidArgumentException('Canonical account code is required');
+        }
+        if (!DkCanonicalDecimal::equals($this->debit, '0') && !DkCanonicalDecimal::equals($this->credit, '0')) {
+            throw new InvalidArgumentException('A canonical line cannot have both debit and credit amounts');
+        }
+    }
+}
