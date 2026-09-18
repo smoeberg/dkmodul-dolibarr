04a5899de3f1c3e2f27fad928b23041cca8ddd3e
WM-group development - Eira <moeberg@wm-group.dk>
2026-09-18 14:07:38 +0200

    test: make SAF-T exporter unit test self-contained

--- tests/unit/Saft21ExporterTest.php
+++ tests/unit/Saft21ExporterTest.php
@@ -1,3 +1,30 @@
 <?php
 
-require_once __DIR__.'/../integration/validate-saft21.php';
+require_once __DIR__.'/../fixtures/SaftFixtureProvider.php';
+require_once __DIR__.'/../../dkmodul/class/Saft/V21/Saft21Exporter.php';
+
+$provider = new DkSaftFixtureProvider();
+$exporter = new DkSaft21Exporter($provider);
+$xml = $exporter->export('2026-01-01', '2026-12-31', array(
+    'createdDate' => '2026-09-18',
+    'softwareCompanyName' => 'Dolibarr DK',
+    'softwareId' => 'Dolibarr DK',
+    'softwareVersion' => '0.1.0-dev',
+    'taxEntity' => '12345678',
+));
+
+$dom = new DOMDocument();
+assert($dom->loadXML($xml) === true);
+
+$xpath = new DOMXPath($dom);
+$xpath->registerNamespace('saf', DkSaftSchemaRegistry::NAMESPACE_URI);
+
+assert($xpath->evaluate('string(/saf:AuditFile/saf:Header/saf:AuditFileVersion)') === '2.1');
+assert($xpath->evaluate('string(/saf:AuditFile/saf:Header/saf:Company/saf:CVR)') === '12345678');
+assert($xpath->evaluate('string(/saf:AuditFile/saf:MasterFiles/saf:GeneralLedgerAccounts/saf:VersionOfStandardAccount)') === '20260101');
+assert($xpath->evaluate('string(/saf:AuditFile/saf:GeneralLedgerEntries/saf:NumberOfEntries)') === '2');
+assert($xpath->evaluate('string(/saf:AuditFile/saf:GeneralLedgerEntries/saf:TotalDebit)') === '250.00000000');
+assert($xpath->evaluate('string(/saf:AuditFile/saf:GeneralLedgerEntries/saf:TotalCredit)') === '250.00000000');
+assert($xpath->evaluate('string(/saf:AuditFile/saf:GeneralLedgerEntries/saf:Journal/saf:Description)') === 'Diverse posteringer');
+
+echo "SAF-T 2.1 exporter unit tests passed\n";
